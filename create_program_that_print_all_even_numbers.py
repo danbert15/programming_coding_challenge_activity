caf7da84a1b335bfa73a5a4a1e937314c4d00e7c
Dan <danbertbacer@gmail.com>
2026-03-12 21:54:16 +0800

    Print all even Numbers Code

---
+++ create_program_that_print_all_even_numbers.py
@@ -0,0 +1,12 @@
+#Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
+
+#First Option (Uncomment the code to work):
+
+# for i in range(0, 101, 2):
+#     print(i)
+
+#Other ways (Uncomment the code to work):
+
+# for i in range(101):
+#     if i % 2 == 0:
+#         print(i)
