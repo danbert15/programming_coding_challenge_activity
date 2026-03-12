b2e5ef30655b1e6a57023ea6d5e6b4b9704d6168
Dan <danbertbacer@gmail.com>
2026-03-12 20:45:12 +0800

    Print Equal Numbers Code

---
+++ create_program_that_ask_user_print_equal_when_the_numbers_are_the_same.py
@@ -0,0 +1,9 @@
+#Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+if number1 == number2:
+    print("Both numbers are equal")
+else:
+    print("Both numbers are not equal")
