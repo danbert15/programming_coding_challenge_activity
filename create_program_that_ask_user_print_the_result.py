9290d63fd75dd7bae6bc72d5f90adb55ebdfe53c
Dan <danbertbacer@gmail.com>
2026-03-12 21:23:01 +0800

    Print Result Code

---
+++ create_program_that_ask_user_print_the_result.py
@@ -0,0 +1,7 @@
+#Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+result = number1 ** number2
+print("The result when the first number is raised to the second number is: ", result)
