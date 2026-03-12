db0703528463564f2216c2d1f77cf7419c70ec6d
Dan <danbertbacer@gmail.com>
2026-03-12 21:01:43 +0800

    Print Sum Code

---
+++ create_program_that_ask_user_print_sum_of_the_two_numbers.py
@@ -0,0 +1,7 @@
+#Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
+
+number1 = float(input("Enter the first number: "))
+number2 = float(input("Enter the second number: "))
+
+sum_numbers = number1 + number2
+print("The sum of the two numbers is: ", sum_numbers)
