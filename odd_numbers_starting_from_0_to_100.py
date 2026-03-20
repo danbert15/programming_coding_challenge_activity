c2457819fe6f171145337d2a024e61f8c232ecc4
Dan <danbertbacer@gmail.com>
2026-03-20 12:31:12 +0800

    prints all the odd numbers from 0 to 100

---
+++ odd_numbers_starting_from_0_to_100.py
@@ -0,0 +1,8 @@
+#Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
+
+num = 0
+
+while num <= 100:
+    if num % 2 != 0:
+        print(num)
+    num += 1
