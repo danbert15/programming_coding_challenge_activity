aea5164e96402927cf5fb197f9b89ee5c72058fb
Dan <danbertbacer@gmail.com>
2026-04-08 15:47:37 +0800

    prints the number in 6 digits format

---
+++ numbers_0_to_1000.py
@@ -0,0 +1,7 @@
+#Prog02: Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
+
+number = int(input("Enter a number (0-1000): "))
+
+formatted = str(number).zfill(6)
+
+print("Output:", formatted)
