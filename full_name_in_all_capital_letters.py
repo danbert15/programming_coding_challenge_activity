dd8e0a61279a793dfde2a78bf49d6e106282d47b
Dan <danbertbacer@gmail.com>
2026-04-08 15:50:39 +0800

    prints the full name in all capital letters

---
+++ full_name_in_all_capital_letters.py
@@ -0,0 +1,7 @@
+#Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
+
+fullname = input("Enter your full name: ")
+
+upper_name = fullname.upper()
+
+print("Output:", upper_name)
