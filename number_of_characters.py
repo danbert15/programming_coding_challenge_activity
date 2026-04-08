b3bb95597d8ec1c299ae3a6b2d1abbb1fcdcf39a
Dan <danbertbacer@gmail.com>
2026-04-08 16:12:09 +0800

    prints the number of characters of fullname

---
+++ number_of_characters.py
@@ -0,0 +1,7 @@
+#Prog08: Create a program that ask the user to input their fullname. Print the number of characters in the input.
+
+fullname = input("Enter your full name: ")
+
+char_count = len(fullname)
+
+print("Number of characters:", char_count)
