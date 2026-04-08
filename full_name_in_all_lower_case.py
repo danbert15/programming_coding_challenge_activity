dcbb0832ded84aca523db14b9d16c7278d908486
Dan <danbertbacer@gmail.com>
2026-04-08 15:53:39 +0800

    prints the full name in all lower case

---
+++ full_name_in_all_lower_case.py
@@ -0,0 +1,7 @@
+#Create a program that ask the user to input their fullname. Print the input in all lower case.
+
+fullname = input("Enter your full name: ")
+
+lower_name = fullname.lower()
+
+print("Output:", lower_name)
